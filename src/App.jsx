d7e5abcb54348e595d2d59428dd27ea6aa9cb409
Andres Davila <andresdavilasanchez@gmail.com>
2024-10-06 16:32:26 -0500

    Introduction to Props: adding functionality to shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,71 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
+import { db } from "./data/db"
 
 function App() {
-
     // Hooks: se ponen al inicio, nunca dentro de condicionales
-    // UseState: [state, función que modifica al state] = useState(valor inicial)
-    const [auth, setAuth] = useState(true)
-    const [total, setTotal] = useState(0)
-    const [cart, setCart] = useState([])
 
-    //useEffect: ejecutar componte cuando  esta listo (API o localstorage)
-    // tiene callback (funcion que se ejecuta cuando componente esta listo)
-    // tiene array de dependnecias: se ejecuta cuando los elementos del array estan lito
-    useEffect(() => {
-      console.log('Componente listo')
-    }, [])
+    // - useState: [state, función que modifica al state] = useState(valor inicial)
+
+    // - useEffect: ejecutar componte cuando está listo (API o localstorage), tiene:
+        // callback: función (arrow) que se ejecuta cuando una dependencia cambia su state
+        // array de dependencias: va a estar escuchando los cambios de estas variables/states
+        // si array de dependencia está vacío: se ejecuta 1 sola vez cuando el componente está listo
+	
+    // Props: compartir información entre componentes (padre->hijo): <Componente prop={valor} />
+
+	// inicio data como un componente vacío
+	// eslint-disable-next-line no-unused-vars
+	const [data, setData] = useState(db) // normalmente se inicializa el state con vacío/false/etc...
+    const [cart, setCart] = useState([]) // state para el carrito de compras
+	
+    // agrega al carrito: lógica para manejar cantidad de items repetidos, etc
+    function addToCart(item) {
+
+        //verificar si el item existe: si no existe (-1) si sí existe, retorna el primer índice donde aparece
+        const itemExists = cart.findIndex((currentItem) => currentItem.id === item.id)
+
+        if(itemExists >= 0) { // existe en el carrito
+            const updatedCart = [...cart] // copia del state
+            updatedCart[itemExists].quantity++ // actualizar cantidad
+            setCart(updatedCart) //agregar el item con cantidad actualizada al state
+        }
+        else {
+            item.quantity = 1 // agregando propiedad nueva mostrando la cantidad
+            setCart([...cart, item]) // agrega el nuevo item al carrito
+        }
+    }
 
     return (
     <>
-
-    <Header/>
+    <Header
+        cart = {cart}    
+    />
 
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            <Guitar/>
+			{data.map((guitar) => (
+					<Guitar
+                        //prop = valor (mismo nombre)
+                        key={guitar.id} // SIEMPRE debo pasar como prop un id cuando uso MAP
+						guitar = {guitar} 
+                        setCart = {setCart}
+                        addToCart = {addToCart}
+					/>
+				)
+			)}
+			
         </div>
     </main>
-
 
     <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
             <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
         </div>
     </footer>
-
     </>
   )
 }
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,16 +1,30 @@
-export default function Guitar() {
+// eslint-disable-next-line react/prop-types
+export default function Guitar({guitar, addToCart}) {
+    
+    // Props: propiedades que salen del componente padre (App)
+    // eslint-disable-next-line react/prop-types, no-unused-vars
+    const { id, name, image, description, price } = guitar
+
+    // Obsoleto: no es necesario crear una función adicional
+    // const handleClick = (guitar) => {
+    //     setCart([...cart, guitar]) // al cart, le seteo con el cart anterior + el elemento nuevo
+    // }
+
     return(
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
                 <div className="col-4">
-                    <img className="img-fluid" src="./img/guitarra_01.jpg" alt="imagen guitarra" />
+                    <img className="img-fluid" src={`/img/${image}.jpg`} alt="imagen guitarra" />
                 </div>
                 <div className="col-8">
-                    <h3 className="text-black fs-4 fw-bold text-uppercase">Lukather</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p className="fw-black text-primary fs-3">$299</p>
+                    <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+                    <p>{description}</p>
+                    <p className="fw-black text-primary fs-3">${price}</p>
                     <button 
                         type="button"
                         className="btn btn-dark w-100"
+                        //usar arrow function para que la función SOLO se llame en evento, no automáticamente
+                        // puedo no pasar cart como prop, sino usar un parámetro de setCart (automáticamente será cart) y no definir ninguna función handleClick adicional
+                        onClick={() => addToCart(guitar)}
                     >Agregar al Carrito</button>
                 </div>
             </div>
